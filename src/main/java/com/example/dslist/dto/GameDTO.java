ed3b3ab8fbaf63e8d52a6ca118e1ddb471aad50d
Maicon8Matos <maicondossantosmatos1995@gmail.com>
2025-05-22 00:31:18 -0400

    GET game by id

---
+++ src/main/java/com/example/dslist/dto/GameDTO.java
@@ -0,0 +1,105 @@
+package com.example.dslist.dto;
+
+import com.example.dslist.entities.Game;
+
+public class GameDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDesciption;
+	private String longDescription;
+	
+	public GameDTO() {}
+
+	public GameDTO(Game entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.year = entity.getYear();
+		this.genre = entity.getGenre();
+		this.platforms = entity.getPlatforms();
+		this.score = entity.getScore();
+		this.imgUrl = entity.getImgUrl();
+		this.shortDesciption = entity.getShortDescription();
+		this.longDescription = entity.getLongDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDesciption() {
+		return shortDesciption;
+	}
+
+	public void setShortDesciption(String shortDesciption) {
+		this.shortDesciption = shortDesciption;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	
+	
+}
--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.repositories.GameRepository;
@@ -15,8 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> games = gameRepository.findAll();
 		return games.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id){
+		Game game = gameRepository.findById(id).get();
+		return new GameDTO(game);
 	}
 }
